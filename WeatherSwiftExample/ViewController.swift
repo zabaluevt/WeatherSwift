2507cc1d6464c3d5cd67f54b4da873ff15c639d8
Timofey Zabaleuv <zabti@yandex.ru>
2018-11-22 17:53:17 +0400

    Initial Commit

---
+++ WeatherSwiftExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherSwiftExample
+//
+//  Created by Ольга Забалуева on 22.11.2018.
+//  Copyright © 2018 Тимофей Забалуев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
