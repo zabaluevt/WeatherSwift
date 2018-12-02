dc6078d8edcaa57f292bdfd2cf79afddb540a038
Timofey Zabaleuv <zabti@yandex.ru>
2018-12-02 14:36:07 +0400

    First commit

---
+++ WeatherSwiftExample/Model/JsonResponse.swift
@@ -0,0 +1,61 @@
+//
+//  TopAnswerers.swift
+//  WeatherSwiftExample
+//
+//  Created by Ольга Забалуева on 26.11.2018.
+//  Copyright © 2018 Тимофей Забалуев. All rights reserved.
+//
+
+import Foundation
+
+struct JsonResponse: Codable {
+    let coord: Coord?
+    let weather: [Weather]?
+    let base: String?
+    let main: Main?
+    let visibility: Int?
+    let wind: Wind?
+    let clouds: Clouds?
+    let dt: Int?
+    let sys: Sys?
+    let id: Int?
+    let name: String?
+    let cod: Int?
+}
+
+struct Clouds: Codable {
+    let all: Int?
+}
+
+struct Coord: Codable {
+    let lon, lat: Double?
+}
+
+struct Main: Codable {
+    let temp: Double?
+    let pressure, humidity: Int?
+    let tempMin, tempMax: Double?
+
+    enum CodingKeys: String, CodingKey {
+        case temp, pressure, humidity
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+    }
+}
+
+struct Sys: Codable {
+    let type, id: Int?
+    let message: Double?
+    let country: String?
+    let sunrise, sunset: Int?
+}
+
+struct Weather: Codable {
+    let id: Int?
+    let main, description, icon: String?
+}
+
+struct Wind: Codable {
+    let speed: Double?
+    let deg: Double?
+}
